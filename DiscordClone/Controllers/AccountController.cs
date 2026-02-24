a57ef1ca4f7506c55dd38134e876d089508dcf35
BeckyWinkler <beccavillwock@yahoo.com>
2026-02-23 20:09:47 -0600

    can now add new users

--- DiscordClone/Controllers/AccountController.cs
+++ DiscordClone/Controllers/AccountController.cs
@@ -1,17 +1,42 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using DiscordClone.Models;
+using System.Data.Common;
 
 namespace DiscordClone.Controllers;
 
 public class AccountController : Controller
 {
+    //database connection
+    private readonly ApplicationDbContext _context;
+
+    public AccountController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
     public IActionResult Login()
     {
         return View();
     }
-     public IActionResult SignUp()
+
+    //display sign up form
+    [HttpGet]
+    public IActionResult SignUp()
     {
+        
         return  View();
+    }
+    //handle form submission
+    [HttpPost]
+     public IActionResult SignUp(AppUser appUser)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.AppUsers.Add(appUser);
+            _context.SaveChanges();
+            return RedirectToAction("Login");
+        }
+        return  View(appUser);
     }
 }
